ce9449fc2608ce6737879d307f0669cc2a51977a
Crowdbotics <team@crowdbotics.com>
2020-12-05 20:20:06 +0000

    Initial commit.

---
+++ ios/promo_easy_23205Tests/promo_easy_23205Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface promo_easy_23205 : XCTestCase
+
+@end
+
+@implementation promo_easy_23205
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
